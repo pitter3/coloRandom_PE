249cd79dab120504e42653174ec140ee1c4192e6
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-07-26 13:47:43 -0600

    Feat: Function to update data model with array of hex codes

--- name.js
+++ name.js
@@ -1,4 +1,10 @@
+//Update data model so we can access hex codes in squares and captions
+
+
+
 var hexCharacters = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+var hexArray = [];
+
 function randomHex(hexCharacters){
 var hexArray = [];
 for(var i=0; i<6; i++){
@@ -9,3 +15,17 @@ for(var i=0; i<6; i++){
   var hexCode = `#${hexArray.join("")}`;
   return hexCode
 }
+
+
+//REFACTOR WITH FOR LOOP
+function randomPalette(){
+    hexArray.push(randomHex(hexCharacters));
+    hexArray.push(randomHex(hexCharacters));
+    hexArray.push(randomHex(hexCharacters));
+    hexArray.push(randomHex(hexCharacters));
+    hexArray.push(randomHex(hexCharacters));
+    return hexArray
+    }
+
+
+console.log(randomPalette())
