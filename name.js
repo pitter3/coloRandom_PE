88ff3fc72e45102ba59c504710d04388325d3e7b
Jonathan Alfonso <jkentalfonso@gmail.com>
2023-07-26 12:56:43 -0600

    Write data model and function to produce random hex codes.

--- name.js
+++ name.js
@@ -1,39 +1,11 @@
-// ITERATION 0:
-
-// HTML to layout the structure of the page. We need a HEADER(h1), MAIN<main>.
-// Question: How do we turn a randomly generated number into a hex code?
-// Use flexbox to orgnaize Header and Main.
-// Can we use labels (like we did with radio buttons) for our color box text display.
-// 
-
-// ITERATION 1: 
-
-// Button shape is weird-- we will have to use border radius.
-// Data model for our hex codes.
-// We will need the to index our data model and set colors based on its contents.
-// Each box will represent an index position in our data model array. Box 1 is 0, Box 2 is 1.....
-// We will need have each box read its index position and set itself to the color it is reading.
-
-// ITERATION 2:
-
-// We will need to add images to our boxes that have event listeners on click.
-// A conditional that only clears our data model if the box is unlocked.
-// If box is locked do not clear out the data model in that index position.
-// Question: Can we use a for loop + a conditional for this?
-
-// ITERATION 3: 
-
-// Build Saved palettes section with CSS + HTML 
-// Question: is this an HTML aside??
-// New Data Model array for saved palettes section
-// Event Listener on Save Palette button to push original Data Model contents into new one.
-// Style mini boxes
-// !!similar to savedCovers from our paired project!!
-
-// ITERATION 4:
-
-// Splice our data model when we click the X.
-// Add the X next to our saved palettes.
-// Add event listener to the X.
-// Remove palette tied to the proper X.
-
+var hexCharacters = ["a", "b", "c", "d", "e", "f", "0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
+function randomHex(hexCharacters){
+var hexArray = [];
+for(var i=0; i<6; i++){
+  var randomIndex =  Math.floor(Math.random() * 16);
+  var randomCharacter = hexCharacters[randomIndex];
+  hexArray.push(randomCharacter);
+}
+  var hexCode = `#${hexArray.join("")}`;
+  return hexCode
+}
